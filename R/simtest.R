b5414a5293964c584580801906f095612fb64cca
Mackson Ncube <macksonncube@gmail.com>
2021-11-17 10:25:44 -0800

    update comments

--- R/simtest.R
+++ R/simtest.R
@@ -1,6 +1,6 @@
 simtest <- function(test = function(x) sum(x) >= 3, on = c(0, 1), reps = 10, sims = 10^2, replace = TRUE) {
 
-  #Game
+  #Test
   tester <- function(reps){
     sample_test <- sample(on, reps, replace = replace)
     test(sample_test)
